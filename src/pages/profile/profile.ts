6710e47704d2a281993a4f6d3b1420e274d4290f
Stefan Golubovic <golub1994@gmail.com>
2017-04-27 21:28:07 +0200

    Update ProfilePage to support FB silent login

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {NavController, NavParams, PopoverController} from 'ionic-angular';
+import {AlertController, NavController, NavParams, PopoverController} from 'ionic-angular';
 import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook';
 import {PopoverPage} from "../popover/popover";
 
@@ -23,11 +23,30 @@ export class ProfilePage {
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
               public popoverCtrl: PopoverController,
+              public alertCtrl: AlertController,
               private fb: Facebook) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Profile');
+    this.fbSilentLogin();
+  }
+
+  fbSilentLogin() {
+    this.fb.getLoginStatus().then(data => {
+      if (data.status == "connected") {
+        this.setUserData(data);
+      }
+    });
+  }
+
+  setUserData(data: any) {
+    this.userProfile["id"] = data.authResponse.userID;
+    this.userProfile["access_token"] = data.authResponse.accessToken;
+    // load user 'friends'
+    this.getUserData();
+
+    this.loggedIn = true;
   }
 
   fbLogin() {
@@ -35,45 +54,25 @@ export class ProfilePage {
 
     this.fb.login(['public_profile', 'user_friends', 'email'])
       .then((res: FacebookLoginResponse) => {
-        console.log('Logged into Facebook!', res);
-        this.loggedIn = true;
-
-        this.fb.api('/' + res.authResponse.userID + '?fields = id,name,email', []).then((data) => {
-          //Get the user data
-          this.userProfile = {
-            access_token: res.authResponse.accessToken,
-            name: data.name,
-            id: data.id,
-            email: data.email,
-            birthday: ''
-          };
-          //Get the user profile picture and save in user object
-          this.fb.api('/' + res.authResponse.userID + '/picture?height=150&width=150&redirect=false', []).then((data) => {
-            this.userProfile['profile_photo'] = data.data.url;
-            //Get the user friends (using app)
-            this.fb.api('/' + res.authResponse.userID + '/friends', []).then((data) => {
-              this.userProfile['friends'] = data.data;
-              this.getPictures();
-            });
-          });
-
-        });
+        this.setUserData(res);
       })
       .catch(e => console.log('Error logging into Facebook', e));
+  }
+
+  getUserData() {
+    this.fb.api('/' + this.userProfile.id + '?fields = name', []).then((data) => {
+      //Get the user data
+      this.userProfile['name'] = data.name;
+      //Get the user profile picture and save in user object
+      this.fb.api('/' + this.userProfile.id + '/picture?height=150&width=150&redirect=false', []).then((data) => {
+        this.userProfile['profile_photo'] = data.data.url;
+      });
+    });
   }
 
   fbLogout() {
     this.fb.logout();
     this.loggedIn = false;
-  }
-
-  getPictures() {
-    for (let friend of this.userProfile.friends) {
-      this.fb.api('/' + friend.id + '/picture?redirect=false', [])
-        .then((data) => {
-          friend['profile_photo'] = data.data.url;
-        });
-    }
   }
 
   edit() {
@@ -88,7 +87,20 @@ export class ProfilePage {
     this.userProfile.birthday = this.birthdayBeforeEdit;
   }
 
-  callOnMe() { alert('party')}
+  checkEmail() {
+    let regexp = new RegExp('^[a-zA-Z0-9_\.]+@[a-zA-Z0-9]+\.[a-z]{2,3}$');
+    if (this.userProfile.email == '' || regexp.test(this.userProfile.email)) {
+      this.canEdit = false;
+    }
+    else {
+      let alert = this.alertCtrl.create({
+        title: 'Error!',
+        subTitle: "Invalid email address!",
+        buttons: ['OK']
+      });
+      alert.present();
+    }
+  }
 
   more(myEvent) {
     let popover = this.popoverCtrl.create(PopoverPage);
